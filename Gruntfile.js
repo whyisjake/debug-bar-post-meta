7129ba2355f6a7f0750809e9e7038ae50f286d77
Jake Spurlock <whyisjake@gmail.com>
2020-08-18 16:08:36 -0700

    Add all the files

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+      wp_readme_to_markdown: {
+          your_target: {
+            files: {
+                'readme.md': 'readme.txt'
+              }
+          },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+    grunt.registerTask('default', ['wp_readme_to_markdown']);
+
+  };
